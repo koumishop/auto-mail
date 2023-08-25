eab2a8d3fbcf72cfe406966d605e89df0bfe01e8
koumishop15 <koumishop@gmail.com>
2023-08-25 12:23:48 +0100

    refactor: transform card into a component

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,14 @@
-export default function Card({children}) {
+import Lottie from 'react-lottie'
+
+export default function Card({title, defaultOptions}) {
     return(
-        <div className="md:w-[230px] md:h-[259px] mx-4 mb-8 flex flex-col items-center justify-start rounded-2xl bg-white shadow-md hover:scale-110">
-            {children}
+        <div className="md:w-[190px] md:h-[213px] mx-3 mb-4 flex flex-col items-center justify-start rounded-2xl bg-white shadow-md hover:scale-105">
+            <div className="mt-5 mb-2">
+                <Lottie options={defaultOptions} height={85} width={85} />
+            </div>            
+            <h3 className="text-[#343E61] w-[160px] px-5 md:px-0 mb-8 text-base font-semibold">
+                {title}
+            </h3>
         </div>
     )
 }
